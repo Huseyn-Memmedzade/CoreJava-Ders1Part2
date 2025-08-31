315d7304e0a1784b824810ffefc2800977692623
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-08-31 16:25:25 +0400

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,17 @@
+package yenipabka;
+
+public class DataTypesDemo {
+
+    public static void main(String[] args) {
+
+        int huseyn = 28;
+        double pi = 3.14;
+        String ad = "Eldar";
+
+        System.out.println(huseyn);
+        System.out.println(pi);
+        System.out.println(ad);
+    }
+
+
+}
